d6472d01ccb8ee3d4410773c9a4971ff50539d17
nyy28477590 <62798244+nyy28477590@users.noreply.github.com>
2022-03-18 15:18:30 +0800

    Add files via upload

---
+++ getVote.js
@@ -0,0 +1,46 @@
+function getVoteCount(){
+    let url = "https://event.momoshop.com.tw/CloudVote.PROMO";
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+    
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    
+    
+        let data = `{
+        "doAction": "voteCnt",
+        "no": "A_7",
+        "promoNo": "P020220318"
+        }`;
+        xhr.send(data);
+    
+        xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            var res = JSON.parse(xhr.responseText)
+            var countNum = (res.competitorStatus[0].TOTALSCORE);
+            console.log(countNum)
+            document.querySelector(".voteCount > .value").innerHTML=countNum+" votes"
+        }};
+    var timeinterval = setInterval(function(){
+        let url = "https://event.momoshop.com.tw/CloudVote.PROMO";
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+    
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    
+    
+        let data = `{
+        "doAction": "voteCnt",
+        "no": "A_7",
+        "promoNo": "P020220318"
+        }`;
+        xhr.send(data);
+    
+        xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            var res = JSON.parse(xhr.responseText)
+            var countNum = (res.competitorStatus[0].TOTALSCORE);
+            console.log(countNum)
+            document.querySelector(".voteCount > .value").innerHTML=countNum+" votes"
+        }};
+    },10000);
+};
